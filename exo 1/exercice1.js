d20d893892d11dbaa6f1c12057745ad904e7cf1e
coque <coquelet.samuel@gmail.com>
2021-11-18 10:23:43 +0100

    fini

--- exo 1/exercice1.js
+++ exo 1/exercice1.js
@@ -1,5 +1,8 @@
 let avatarEls = document.getElementsByTagName("img");
 
+for(let i = 0 ;i < avatarEls.length; i++) {
+    avatarEls[i].style.height = '60px';
+}
 
 
 
--- exo 2/exercice.js
+++ exo 2/exercice.js
@@ -2,7 +2,12 @@ let colorEl = document.getElementById("trendy-color");
 let handEl  = document.getElementById("trendy-handwriting");
 let frameEl = document.getElementById("trendy-frame");
 
+colorEl.innerHTML = 'red';
+colorEl.style.color = 'red';
 
+handEl.innerHTML = 'boss';
+
+frameEl.style.border = '1px solid black';
 
 
 
